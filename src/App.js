74c67c8cd854ca35f42a29b72387eed7ecd0e493
nishit-g <nishit.gupta6708@gmail.com>
2020-12-29 09:07:05 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
       <h1>Hellow React</h1>
+      <Player />
+      <Song />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>player</h1>
+    </div>
+  );
+};
+
+export default Song;
